81dd4688e0dc0989bcf083c973918cb6560d01ef
marthy123mm <ing.martin.ayon@outlook.com>
2017-12-29 09:40:50 -0600

    Configuring MYSQL database User Access

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=all_inventory',
     'username' => 'root',
-    'password' => '',
+    'password' => 'sql123',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
