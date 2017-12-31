a1363c9b4160a6c19dff384cd3aa9721714748e7
marthy123mm <ing.martin.ayon@outlook.com>
2017-12-31 12:18:53 -0600

    Addin translation es-MX for Asset interface

--- messages/es-MX/app.php
+++ messages/es-MX/app.php
@@ -5,7 +5,21 @@
 */
 return [
     'wellcome' => 'Bienvenido',
+    
+    //TRASNLATIONS FOR ACTIVOS
     'Assets' => 'Activos',
+    'Id Asset' => 'ID de activo',
+    'Create Asset' => 'Crear Activo',
+    'Purchase Date' => 'Fecha de Compra',
+    'Description' => 'Descripción',
+    'Sales Check' => 'Factura',
+    'Create At' => 'Creado',
+    'Status' => 'Estado',
+    'Serial Number' => 'Número de serie',
+    'Ubication' => 'Ubicación',
+    'Id Asset Type' => 'Id Tipo de Activo',
+	'Id Model' => 'Id Modelo',
+	'Id Leasing' => 'Id Arrendamiento',
     
 ];
 
