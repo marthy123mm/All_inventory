1b6fe39c3aacc1f07ad3d0d3148666a993589f7d
marthy123mm <ing.martin.ayon@outlook.com>
2017-12-31 17:21:09 -0600

    Adding translations for Brand model

--- messages/es-MX/app.php
+++ messages/es-MX/app.php
@@ -6,6 +6,8 @@
 return [
     'wellcome' => 'Bienvenido',
     'Save' => 'Guardar',
+    'Update' => 'Actualizar',
+    'Delete' => 'Borrar',
     
     //TRASNLATIONS FOR ACTIVOS
     'Assets' => 'Activos',
@@ -24,7 +26,13 @@ return [
 	'General information' => 'Información General',
 	'Finance information' => 'Información de Finanzas',
 	'Price' => 'Precio',
-    
+
+
+	'Brands' => 'Marcas',
+	'Brand' => 'Marca',
+	'Id Brand' => 'Id de marca',
+    'Create Brand' => 'Crear marca',
+    'Update Brnad' => 'Actualizar marca',
 ];
 
 ?>
