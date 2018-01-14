c3d272cc3e8c2abe8d0011909ed0a5358ab9bd41
marthy123mm <ing.martin.ayon@outlook.com>
2018-01-14 10:48:49 -0600

    Adding component to form

--- views/os/_form.php
+++ views/os/_form.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
+use kartik\widgets\FileInput;
 /* @var $this yii\web\View */
 /* @var $model app\models\Os */
 /* @var $form yii\widgets\ActiveForm */
@@ -12,14 +13,41 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'os_name')->textInput(['maxlength' => true]) ?>
+    <div class="container">
+    <div class="row">
+        <div class="col-md-4">
+        	<?= $form->field($model, 'os_name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+    		<?= $form->field($model, 'description')->textarea(['rows' => 2]) ?>
+        </div>
 
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+        <div class="col-md-3">
+        	<div class="form-group">
+			    <?php echo '<label class="control-label">Imagen</label>';
+				echo FileInput::widget([
+			    	'name' => 'image_os',
+
+			    	'options' => [
+			    		'accept' => 'image/*',
+			    		'required' => 'true',
+			    	],
+				]);
+				?>
+			</div>
+
+			<div class="form-group">
+		        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+		    </div>
+
+
+        </div>
     </div>
+	</div>
 
+    
+
+    
+    
     <?php ActiveForm::end(); ?>
 
 </div>
